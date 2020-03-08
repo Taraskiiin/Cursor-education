7f5cbb7e47466e880bf12f0c1f77155638112d32
taraskiiin <taraskravets95@gmail.com>
2020-03-08 16:32:25 +0200

    HW10

--- hw10/hw10.js
+++ hw10/hw10.js
@@ -1,29 +1,19 @@
+const playSound = (soundUrl) => {
+    const audio = new Audio();
+    audio.src = soundUrl;
+    audio.autoplay = true;
+}
 document.addEventListener('keydown', function (event) {
     switch (event.keyCode) {
         case 49: // (key 1)
-            const bearvoice = new Audio();
-            bearvoice.src = 'wav/grizzlybear.wav';
-            bearvoice.autoplay = true;
-            break;
+            return playSound('wav/grizzlybear.wav');
         case 50: // (key 2)
-            const lionvoice = new Audio();
-            lionvoice.src = 'wav/lion.wav';
-            lionvoice.autoplay = true;
-            break;
+            return playSound('wav/lion.wav');
         case 51: // (key 3)
-            const dogvoice = new Audio();
-            dogvoice.src = 'wav/dogwhine.wav';
-            dogvoice.autoplay = true;
-            break;
+            return playSound('wav/dogwhine.wav');
         case 52: // (key 4)
-            const catvoice = new Audio();
-            catvoice.src = 'wav/kittycry.wav';
-            catvoice.autoplay = true;
-            break;
+            return playSound('wav/kittycry.wav');
         case 53: // (key 5)
-            const monkeyvoice = new Audio();
-            monkeyvoice.src = 'wav/monkey1.wav';
-            monkeyvoice.autoplay = true;
-            break;
+            return playSound('wav/monkey1.wav');
     };
 });
