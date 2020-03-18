246497554aa905491b48a2c90df52ba8e3323e9f
taraskiiin <taraskravets95@gmail.com>
2020-03-18 18:22:00 +0200

    HW11

---
+++ hw11/hw11.js
@@ -0,0 +1,29 @@
+const length = Number(prompt('введіть будь ласка довжину', 4));
+const min = 5;
+let interation = null;
+function randomSymbol(min) {
+  let numberGenerator = ((Date.now()).toString()).split([]).reverse();
+  let last5 = [];
+  for (let j = 0; j < min; j++) {
+    last5.push(numberGenerator[j]);
+  }
+  return String.fromCharCode(Number(last5.join('')));
+}
+const error = () => {
+  new Error('Ви зробили фатальну помилку:с');
+};
+const generatePromise = () => {
+	return new Promise(function (resolve) {
+		return setTimeout(() => resolve(randomSymbol(min)), 50);
+	  });
+}
+async function getRandomChinese(length){
+  const randomChinese = [];
+  for (let i = 0; i < length; i++) {
+    randomChinese.push(await generatePromise());
+  };
+  console.log(randomChinese.join());
+}
+getRandomChinese(length);
+
+
