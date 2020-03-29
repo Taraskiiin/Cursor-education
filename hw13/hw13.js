0b7c1a7656feb4c2e65e861bf0fe9d2125883e43
taraskiiin <taraskravets95@gmail.com>
2020-03-29 19:26:54 +0300

    hw13advanced

--- hw13/hw13.js
+++ hw13/hw13.js
@@ -13,3 +13,26 @@ const getJellyfish = () => {
     <p>Отримано ${idGenerator.next().value} кілограмів гречки</p>`;
     container.append(resDiv);
 }
+const fontDefault = 24;
+const max = 30;
+const min = 24;
+function* ChangeFS(fontSize){
+    while(true){
+        let size = yield fontSize;
+        if (size === 'up'){
+            fontSize += 3;
+        }
+        if (size === 'down'){
+            fontSize -= 3;
+        }
+    }
+}
+const fontGenerator = ChangeFS(fontDefault);
+const upFont = () => { 
+    const result = fontGenerator.next('up').value;
+    document.getElementById("text").style.fontSize = result + "px";
+};
+const downFont = () => { 
+    const result = fontGenerator.next('down').value;
+    document.getElementById("text").style.fontSize = result + "px";
+};
